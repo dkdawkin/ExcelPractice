77828abe23b0ad3bbf6720b0639c0e8548940599
DkdawkinWin <34441177+dkdawkin@users.noreply.github.com>
2018-02-25 21:07:41 -0500

    Initial Commit

--- ExcelPractice/Program.cs
+++ ExcelPractice/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OfficeOpenXml;
 
 namespace ExcelPractice
 {
@@ -10,6 +11,38 @@ namespace ExcelPractice
     {
         static void Main(string[] args)
         {
+            Program program = new Program();
+            Console.Write(program.readExcel("C:/Users/Delante/Desktop/Schedule.xlsx").ToString());
+            Console.WriteLine("test successful");
+
+        }
+        public int readExcel(String fileName)
+        {
+            int workSheets = 0;
+            try
+            {
+                ExcelPackage excel = new ExcelPackage(new System.IO.FileInfo(fileName));
+                ExcelWorksheets wrkSheets = excel.Workbook.Worksheets;
+                foreach (ExcelWorksheet wrkSheet in wrkSheets)
+                {
+                    for (int i = wrkSheet.Dimension.Start.Row; i <= wrkSheet.Dimension.End.Row;i++)
+                    {
+                        for (int j = wrkSheet.Dimension.Start.Column;j <= wrkSheet.Dimension.End.Column;j++)
+                        {
+                            object cellValue = wrkSheet.Cells[j,i].Value;
+                            Console.Write(" "+cellValue.ToString()+" ");
+                        }
+                        Console.WriteLine("");
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return workSheets;
         }
     }
 }
